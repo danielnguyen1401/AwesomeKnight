66bc741641cd85ff1ffac82ac848356efce5f38d
Yuan <nguyen.leenguyen@gmail.com>
2017-11-11 12:03:11 +0700

    Not finish Player Attack Effect

--- Assets/Scripts/Player/PlayerAttackEffect.cs
+++ Assets/Scripts/Player/PlayerAttackEffect.cs
@@ -2,6 +2,18 @@
 
 public class PlayerAttackEffect : MonoBehaviour
 {
+    [SerializeField] Transform _groundImpactSpawn;
+    [SerializeField] Transform _kickSpawn;
+    [SerializeField] Transform _fireTornadoSpawn;
+    [SerializeField] Transform _fireShieldSpawn;
+
+    [SerializeField] GameObject _groundImpactFx;
+    [SerializeField] GameObject _kickFx;
+    [SerializeField] GameObject _fireTornadoFx;
+    [SerializeField] GameObject _fireShieldFx;
+    [SerializeField] GameObject _healFx;
+    [SerializeField] GameObject _thunderAttackFx;
+
     void Start()
     {
     }
@@ -12,22 +24,33 @@ public class PlayerAttackEffect : MonoBehaviour
 
     void GroundImpact()
     {
+        Instantiate(_groundImpactFx, _groundImpactSpawn.position, Quaternion.identity);
     }
 
     void Kick()
     {
+        Instantiate(_kickFx, _kickSpawn.position, Quaternion.identity);
     }
 
     void FireTornado()
     {
+        Instantiate(_fireTornadoFx, _fireTornadoSpawn.position, Quaternion.identity);
     }
 
     void FireShield()
     {
+//        GameObject fireObj = Instantiate(_fireShieldFx, _fireShieldSpawn.position, Quaternion.identity) as GameObject;
+//        fireObj.transform.SetParent(transform);
+        Instantiate(_fireShieldFx, _fireShieldSpawn.position, Quaternion.identity);
     }
 
     void Heal()
     {
+        Vector3 tempPos = transform.position;
+        tempPos.y += 2f;
+        GameObject healObj = Instantiate(_healFx, tempPos, Quaternion.identity) as GameObject;
+        healObj.transform.SetParent(transform); // Fx will be inside of Black Knight Object
+
     }
 
     void ThunderAttack()
